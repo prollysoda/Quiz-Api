b280d5d2466e067e11b9e5ca37a31cc6ddc38d73
Arbenisya <arbenisya@gmail.com>
2024-01-16 14:01:59 +0700

    first commit

---
+++ models/quiz.js
@@ -0,0 +1,30 @@
+module.exports = (sequielize, Sequelize) => {
+    const Quiz = sequielize.define('quiz', {
+        quiz: {
+            type: Sequelize.STRING,
+        },
+        a: {
+            type: Sequelize.STRING,
+        },
+        b: {
+            type: Sequelize.STRING,
+        },
+        c: {
+            type: Sequelize.STRING,
+        },
+        d: {
+            type: Sequelize.STRING,
+        },
+        key: {
+            type: Sequelize.STRING,
+        },
+        categoryId: {
+            type: Sequelize.INTEGER,
+        },
+        levelId: {
+            type: Sequelize.INTEGER,
+        },
+
+    });
+    return Quiz;
+}
---
+++ router/quiz.js
@@ -0,0 +1,13 @@
+const quizController = require('../controllers/quiz');
+const quiz = require('../models/quiz');
+const router = require('express').Router();
+
+router.post('/', quizController.create);
+router.get('/', quizController.getAll);
+router.get('/:id',quizController.findOne);
+router.put('/:id', quizController.update);
+router.delete('/:id', quizController.delete);
+router.get('/category/:id', quizController.getByCategoryId);
+router.get('/level/:id', quizController.getByLevelId);
+
+module.exports = router;
